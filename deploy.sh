22ef62e6eae4e49494b5bb0d2a21c48fd8bb2488
Andrew Hao <andrewhao@gmail.com>
2015-04-09 13:41:18 -0700

    Updating with nginx proxy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 # docker `docker-machine config` run --link db:db -p 80:3000 -d andrewhao/velocitas-core:latest
-
+# docker-machine active do-prod-velocitas-master01
+# $(docker-machine env do-prod-velocitas-master01)
+# docker-compose up -d
